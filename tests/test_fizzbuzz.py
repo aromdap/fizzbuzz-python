ef8b8df4b8f35c296da9feb9fe34e544994a4e75
michaellennox <michaelctlennox@gmail.com>
2015-12-22 20:40:53 +0000

    Adds failing unit test for when given 3

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -8,3 +8,6 @@ class TestFizzbuzz(unittest.TestCase):
         
     def test_when_given_15_returns_fizzbuzz(self):
         self.assertEqual('fizzbuzz', fizzbuzz(15))
+
+    def test_when_given_3_returns_fizz(self):
+        self.assertEqual('fizz', fizzbuzz(3))
