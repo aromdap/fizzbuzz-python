f149e560f7ebe5e29b58aee655fbe5a0d115cf2a
michaellennox <michaelctlennox@gmail.com>
2015-12-22 20:39:03 +0000

    Adds failing unit test for returning fizzbuzz

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -5,3 +5,6 @@ class TestFizzbuzz(unittest.TestCase):
 
     def test_when_given_5_returns_buzz(self):
         self.assertEqual('buzz', fizzbuzz(5))
+        
+    def test_when_given_15_returns_fizzbuzz(self):
+        self.assertEqual('fizzbuzz', fizzbuzz(15))
